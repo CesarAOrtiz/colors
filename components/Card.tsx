b704190042a55a65b84e72ef8f8bf2536c1d768f
César Ortiz <68305906+CesarAOrtiz@users.noreply.github.com>
2021-11-11 08:57:29 -0400

    Card component added

---
+++ components/Card.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import styles from "./Card.module.css";
+import { Color } from "../utils/colors";
+
+interface CardProps {
+  color: Color;
+}
+
+const Card: NextPage<CardProps> = ({ color }) => {
+  return (
+    <div className={styles.cardContainer}>
+      <div
+        className={styles.cardColor}
+        style={{ backgroundColor: color.hex }}
+      ></div>
+      <div className={styles.cardInfo}>
+        <p>{color.name}</p>
+        <p>{color.hex}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
