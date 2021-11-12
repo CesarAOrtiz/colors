67e1a018c0e97b88116647fa8370e70d5a368eff
César Ortiz <68305906+CesarAOrtiz@users.noreply.github.com>
2021-11-11 20:15:34 -0400

    title class added

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,22 +3,22 @@ import styles from "./Card.module.css";
 import { Color } from "../utils/colors";
 
 interface CardProps {
-  color: Color;
+    color: Color;
 }
 
 const Card: NextPage<CardProps> = ({ color }) => {
-  return (
-    <div className={styles.cardContainer}>
-      <div
-        className={styles.cardColor}
-        style={{ backgroundColor: color.hex }}
-      ></div>
-      <div className={styles.cardInfo}>
-        <p>{color.name}</p>
-        <p>{color.hex}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.cardContainer}>
+            <div
+                className={styles.cardColor}
+                style={{ backgroundColor: color.hex }}
+            ></div>
+            <div className={styles.cardInfo}>
+                <p className={styles.cardInfoTitle}>{color.name}</p>
+                <p>{color.hex}</p>
+            </div>
+        </div>
+    );
 };
 
 export default Card;
